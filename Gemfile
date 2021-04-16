ae46c859b9c53f18bbbaa2a75cfb3143dadb72a8
BhdareshC <wc.bhadresh@gmail.com>
2021-04-16 11:25:04 +0530

    get ready for deploy to herok

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jquery-rails'
 gem 'bcrypt', '~> 3.1.7'
 gem 'hirb'
 
+gem 'mimemagic', github: 'mimemagicrb/mimemagic', ref: '01f92d86d15d85cfd0f20dabd025dcbd36a8a60f'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Evgeny", password: "password")
-User.create(username: "Jonsnow", password: "password")
-User.create(username: "Arya", password: "password")
-User.create(username: "Frodo", password: "password")
-User.create(username: "Gandalf", password: "password")
+User.create(username: "Bhadresh", password: "password")
+User.create(username: "Ashish", password: "password")
+User.create(username: "Maulik", password: "password")
